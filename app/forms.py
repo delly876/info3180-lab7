d1dad475bd9937a4dd0dc28560a42489ff9ed1fe
Delano Senior <dsenior101@hotmail.com>
2021-04-08 00:57:42 -0500

    Exercise 1 Complete

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
+class UploadForm(FlaskForm):
+    description = TextAreaField('Description', validators = [DataRequired()])
+    photo = FileField('Image', validators=[FileRequired(), FileAllowed(['jpg', 'png'], "Please check format, only PNG and JPG images are allowed!")])
